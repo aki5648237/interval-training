41ec689196284ba1f64568261ada6e6bd8ea898c
aki <aki5648237@gmail.com>
2024-03-13 22:51:22 +0900

    3/13 問題のランダム生成作成

--- src/component/intervalTrainingAnswer.tsx
+++ src/component/intervalTrainingAnswer.tsx
@@ -1,14 +1,23 @@
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Question } from './intervalTrainingQuiz';
 
 type Props = {
 	currentQuestion: number
+	questions: Question[]
+	handleAnswerButtonClick: () => void
 }
 
-export const Answer = ({currentQuestion}: Props) => {
+export const Answer = ({currentQuestion, questions, handleAnswerButtonClick}: Props) => {
 	
 	return (
 		<div>
 			<Typography>{currentQuestion+1}問</Typography>
+
+			{/* {questions[currentQuestion].answerOptions.map(item)=> {
+
+			} */}
+			
 
 		</div>
 	)
--- src/component/intervalTrainingQuiz.tsx
+++ src/component/intervalTrainingQuiz.tsx
@@ -1,41 +1,67 @@
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import React,{useState} from 'react';
-import { Answer } from './intervalTrainingAnswer';         
+import { Answer } from './intervalTrainingAnswer'; 
+
+
+
+type AnswerOption ={
+	answerText: string;
+	value: number;
+}
+
+export type Question = {
+  answerOptions: AnswerOption[];
+}
 
 const IntervalTrainingQuiz = () => {
 
-	type questions = [
-		{
-			answerOptions: [
-				{answerText: string,
-				value: number}
-			]
-		}
-	]
+	// 問題名格納
+	let soundName: string = '';
+	// 聞いた回数
+	const [listenCount, setListenCount] = useState<number>(0);
 
-	const questions = [
+
+	// 問題を定数soudNameにランダム格納
+	const getQuiz = (): string => {
+		const rand = Math.floor(Math.random() * questions.length)
+		soundName = questions[1].answerOptions[rand].answerText;
+		const my_audio = new Audio('/' + soundName + '.mp3');
+
+		// クイズ表示時に一回流す
+		my_audio.play();
+		setListenCount(listenCount + 1);
+	
+		return '';
+	
+	}
+
+	const questions: Question[] = [
 		{
 			answerOptions:[
 				{answerText: 'Major 3rd(長3度)', value: 1},
 				{answerText: 'Perfect 5th(完全5度)', value: 2},
 				{answerText: 'Octave(完全8度)', value: 3},
-			]
-		}
-	]
+			],
+		},
+	];
+
+
 
 	// 問題数
 	const [currentQuestion, setCurrentQuestion] =useState<number>(0);
 
-	const handleAnswerButtonClick = () => {
+	const handleAnswerButtonClick = (): void => {
+
+		// if (isCorrect === true) {
+
+		}
 		const nextQuestion: number = currentQuestion + 1;
 
 		if (nextQuestion < questions.length) {
 			setCurrentQuestion(nextQuestion);
 		}
-	}
 	
-
 	return (
 		<div>
 			<Typography variant="h3">
@@ -50,8 +76,9 @@ const IntervalTrainingQuiz = () => {
 
 			<Answer 
 				currentQuestion={currentQuestion}
+				questions={questions}
+				handleAnswerButtonClick={handleAnswerButtonClick}
 			/>
-			
 		</div>
 	)
 }
