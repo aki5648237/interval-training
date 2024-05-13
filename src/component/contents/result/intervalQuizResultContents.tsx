d5589fe5bc7702d922782ce1702510d0668cfa56
aki <aki5648237@gmail.com>
2024-05-13 21:49:40 +0900

    5/13 クイズ結果簡易表示実装

--- src/component/contents/result/intervalQuizResultContents.tsx
+++ src/component/contents/result/intervalQuizResultContents.tsx
@@ -2,35 +2,59 @@ import { useNavigate } from "react-router-dom"
 import React from 'react';
 import { SimpleCalculate } from "../../function/result/intervalQuizResult";
 import { DetailCalculate } from "../../function/result/intervalQuizResult";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 type Props = {
 	handleResetButton : () => void
 	simpleCalculate : SimpleCalculate
 	detailCalculate : DetailCalculate[]
+	openDetailResult : Function
+	closeDetailResult : Function
+	showModal : boolean
 }
 
 export const IntervalQuizResultContents : React.FC<Props> = (
-	{handleResetButton, simpleCalculate, detailCalculate} : Props
+	{handleResetButton, simpleCalculate, detailCalculate, openDetailResult, closeDetailResult, showModal} : Props
 ) => {
 	const navigate = useNavigate();
 	return (
-		<section className="p-result-mv">
-			<div className="p-result-mv-inner">
-				<h2 className="p-result-mv__title">クイズの結果</h2>
-				<div className="p-result-mv__result-box">
-					<div>
-						<ul>
-							<li>得点</li>
-							<li>間違えた回数</li>
-							<li></li>
-						</ul>
+		<div>
+			<section className="p-result-mv">
+				<div className="p-result-mv-inner">
+					<h2 className="p-result-mv__title">クイズの結果</h2>
+					<div className="p-result-mv__result-box">
+						<div>
+							<div className="p-result-mv__total-score">得点 {simpleCalculate.totalScore}/100</div>
+							<ul className="p-result-mv__list-text">
+								<li>間違えた回数 {simpleCalculate.totalMissCount}</li>
+								<li>一発での正答数 {simpleCalculate.perfectQuizNumber}/10</li>
+								<li>リプレイ数 {simpleCalculate.overListenCount}</li>
+							</ul>
+							<div className="p-result-mv__detail-link">
+								<div>詳細な結果はこちら</div>
+								<button onClick={()=> {openDetailResult()}}>
+									<OpenInNewIcon className="p-result-mv__new-icon"/>
+								</button>	
+							</div>
+						</div>
+					</div>
+					<div className="p-result-mv__button-box">
+						<button className="p-result-mv__button-top" onClick={() => navigate('/')} >Top</button>
+						<button className="p-result-mv__button-again" onClick={() => handleResetButton()}>もう一度</button>
 					</div>
 				</div>
-				<div className="p-result-mv__button-box">
-					<button className="p-result-mv__button-top" onClick={() => navigate('/')} >Top</button>
-					<button className="p-result-mv__button-again" onClick={() => handleResetButton()}>もう一度</button>
-				</div>
-			</div>
-		</section>
+			</section>
+			{showModal && (
+				<section className="p-result-detail">
+				<dialog>
+					<div>詳細なクイズ結果</div>
+					<div>詳細なクイズ結果</div>
+					<div>詳細なクイズ結果</div>
+					<div>詳細なクイズ結果</div>
+				</dialog>
+			</section>
+			)}
+		</div>
+		
 	)
 }
--- src/component/function/result/intervalQuizResult.tsx
+++ src/component/function/result/intervalQuizResult.tsx
@@ -2,6 +2,8 @@ import { IntervalQuizResultContents } from "../../contents/result/intervalQuizRe
 import { QuestionResult } from "../quiz/intervalTrainingQuiz"
 import { SimpleResultCalculate } from "./common/simpleResultCalculate"
 import { DetailResultCalculate } from "./common/detailResultCalculate"
+import { useState } from "react"
+
 
 type Props = {
 	handleResetButton : () => void
@@ -27,17 +29,30 @@ export type SimpleCalculate = {
 export const IntervalQuizResult : React.FC<Props> = (
 	{handleResetButton, questionResultList} : Props
 ) => {
+	const [showModal, setShowModal] = useState(false);
 
 	// 簡易計算結果取得
 	const simpleCalculate = SimpleResultCalculate(questionResultList);
 	//  詳細計算結果取得
 	const detailCalculate = DetailResultCalculate(questionResultList);
 
+	// 詳細結果ダイアログ表示
+	const openDetailResult = () => {
+		setShowModal(true);
+		console.log(showModal)
+	}
+	const closeDetailResult = () => {
+		setShowModal(false);
+	}
+
 	return (
 		<IntervalQuizResultContents 
 			handleResetButton={handleResetButton}
 			simpleCalculate={simpleCalculate}
 			detailCalculate={detailCalculate}
+			openDetailResult={openDetailResult}
+			closeDetailResult={closeDetailResult}
+			showModal={showModal}
 		/>
 	)
 }
