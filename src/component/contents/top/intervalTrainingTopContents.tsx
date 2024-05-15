34067fde5e1fb900198e53e49dbbb08b9b45a113
aki <aki5648237@gmail.com>
2024-05-15 22:05:06 +0900

    5/15 クイズ数と種類をtopで選択処理実装

--- src/component/contents/top/intervalTrainingTopContents.tsx
+++ src/component/contents/top/intervalTrainingTopContents.tsx
@@ -1,7 +1,18 @@
 import { useNavigate, } from "react-router-dom"
 import AppHeaderContents from '../common/appHeaderContents';
+import { QuizNumber } from "../../function/top/intervalTraining";
+import { QuizType } from "../../function/top/intervalTraining";
 
-export const IntervalTrainingTopContents = () => {
+type Props = {
+	quizNumber : QuizNumber[]
+  quizType : QuizType[]
+  selectQuizNumber : number
+  selectQuizType : string
+  handleSelectNumberChange : (event : React.ChangeEvent<HTMLSelectElement>) => void
+  handleSelectTypeChange : (event : React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+export const IntervalTrainingTopContents : React.FC<Props> = ({quizNumber, quizType, selectQuizNumber, selectQuizType, handleSelectNumberChange, handleSelectTypeChange} : Props) => {
 	const navigate = useNavigate();
 
 	return (
@@ -15,7 +26,26 @@ export const IntervalTrainingTopContents = () => {
               <div className="p-top-mv__text">  
                 <div>二つの音を聞きインターバルを<br />当てるトレーニングです。</div>
                 <br />
-                <div>問題数は10問です。</div>
+                <div className="p-top-mv__number-box">
+                  <div>問題数の選択 : </div>
+                  <select className="p-top-mv__number-select" value={selectQuizNumber} onChange={(e) => {handleSelectNumberChange(e)}}>
+                    {quizNumber.map((item) => (
+                      <option key={item.option} value={item.quizNumber}>
+                        {item.quizNumber}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="p-top-mv__type-box">
+                  <div>種類の選択 : </div>
+                  <select className="p-top-mv__type-select" value={selectQuizType} onChange={(e) => {handleSelectTypeChange(e)}}>
+                    {quizType.map((item) => (
+                      <option key={item.option} value={item.quizTypeValue}>
+                        {item.quizType}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
               <button className="p-top-mv__button-primary" onClick={() => navigate('/IntervalTrainingQuiz')}>クイズスタート</button>
             </div>
--- src/component/function/top/intervalTraining.tsx
+++ src/component/function/top/intervalTraining.tsx
@@ -1,8 +1,57 @@
+import { useEffect, useState } from "react";
 import IntervalTrainingTopContents from "../../contents/top/intervalTrainingTopContents";
 
+export type QuizNumber = {
+  option : string
+  quizNumber : number
+}
+export type QuizType = {
+  option : string
+  quizType : string
+  quizTypeValue : string
+}
+
 export const IntervalTraining = () => {
+
+  // 問題数の定義
+  const quizNumber : QuizNumber[] = [
+    {option : 'option1', quizNumber : 5},
+    {option : 'option2', quizNumber : 10},
+    {option : 'option3', quizNumber : 20},
+  ]
+  // 種類の定義
+  const quizType : QuizType[] =[
+    {option :'option1', quizType : '3種類', quizTypeValue : '0' },
+    {option :'option1', quizType : '5種類', quizTypeValue : '1' },
+    {option :'option1', quizType : '7種類', quizTypeValue : '2' }
+  ]
+
+  const handleSelectNumberChange = (event : React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectQuizNumber(Number(event.target.value));
+  }
+  const handleSelectTypeChange = (event : React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectQuizType(event.target.value)
+  }
+
+  // 問題数
+  const [selectQuizNumber, setSelectQuizNumber] = useState<number>(0);
+  // 種類
+  const [selectQuizType, setSelectQuizType] = useState<string>('');
+
+  useEffect(() => {
+    console.log(selectQuizNumber)
+
+  }, [selectQuizNumber])
+
   return (
-    <IntervalTrainingTopContents />
+    <IntervalTrainingTopContents 
+      quizNumber={quizNumber}
+      quizType={quizType}
+      selectQuizNumber={selectQuizNumber}
+      selectQuizType={selectQuizType}
+      handleSelectNumberChange={handleSelectNumberChange}
+      handleSelectTypeChange={handleSelectTypeChange}
+    />
 )
 }
 export default IntervalTraining;
